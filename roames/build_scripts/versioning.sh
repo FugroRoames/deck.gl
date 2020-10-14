1122b0856602f13d489c8ab09c4b4255a2977739
Duane van Twest <duanevantwest@roames.com.au>
2020-10-14 13:50:10 +1000

    add roames build directory

---
+++ roames/build_scripts/versioning.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# set -e
+echo "here we go"
+# git version relative to tag, or raw ref as last resort
+VERSION=${VERSION:-$(git describe --long --abbrev=10 --match "HPC-*" 2>/dev/null)}
+VERSION=${VERSION:-$(git describe --long --abbrev=10                 2>/dev/null)}
+VERSION=${VERSION:-$(git describe --long --abbrev=10 --always)}
+echo ${VERSION}
+
+pwd
+
+# reference from repo home dir
+ls modules/main
+
+FILEVERSION="dist.${VERSION}.min.js"
+
+echo ${FILEVERSION}
+
+mv modules/main/dist.min.js modules/main/${FILEVERSION}
+
+
