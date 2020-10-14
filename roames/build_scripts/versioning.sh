ff49e0bf87300e3049dcfe935aa997849bf9fa15
Duane van Twest <duanevantwest@roames.com.au>
2020-10-14 14:59:46 +1000

    debug versioning

--- roames/build_scripts/versioning.sh
+++ roames/build_scripts/versioning.sh
@@ -2,9 +2,12 @@
 
 set +e
 echo "here we go"
+git tag
 # git version relative to tag, or raw ref as last resort
 VERSION=${VERSION:-$(git describe --long --abbrev=10 --match "FRD-*" 2>/dev/null)}
+echo "version is ${VERSION}"
 VERSION=${VERSION:-$(git describe --long --abbrev=10                 2>/dev/null)}
+echo "then version is ${VERSION}"
 VERSION=${VERSION:-$(git describe --long --abbrev=10 --always)}
 echo ${VERSION}
 set -e
