7d5c0159e057b9a34fea224a7b5bdb03d3e903b4
Daiki Ichiyama <d.ichiyama@fugro.com>
2020-11-10 11:37:23 +1000

    DATA-435-render-tiles-3d-to-height-map render with only 1 channel

--- modules/geo-layers/src/boresight-layer/boresight-fs.glsl.js
+++ modules/geo-layers/src/boresight-layer/boresight-fs.glsl.js
@@ -39,11 +39,9 @@ vec4 getLinearColor(float value) {
 void main(void) {
   float weightone = texture2D(textureone, vTexCoords).r;
   float weighttwo = texture2D(texturetwo, vTexCoords).r;
-  float weightonevalid = texture2D(textureone, vTexCoords).g;
-  float weighttwovalid = texture2D(texturetwo, vTexCoords).g;
   // discard pixels with 0 weight.
   // note: height can technically go to negative if rotated in a large angle
-  if (weightonevalid != 1. || weighttwovalid != 1.) {
+  if (weightone <= 0. || weighttwo <= 0.) {
     discard;
   } 
 
