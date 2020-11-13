79af4222b7af9b7f41f742fad2405f1b79657a15
Duane van Twest <duanevantwest@roames.com.au>
2020-11-13 12:42:16 +1000

    modify jenkins deploy script

--- roames/build_scripts/jenkins_deploy.sh
+++ roames/build_scripts/jenkins_deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-#
-# This is script that is used in Jenkins to copy the artifacts from a DeckGL build into the public S3 folder
-#
 
 # lets just see what artifacts were actually copied
 ls
@@ -15,13 +12,17 @@ echo "found ${file_count} js bundles"
 if [ "$file_count" -ne "1" ]; then
     echo "Ambiguous number of javascript mimimified bundles"
     exit 1 # terminate and indicate error
-fi
+fi 
+
 
 # Custom or Prod deploy
 SOFTWARE_PREFIX="software/deckgl"
-if [ -z ${ROAMES_HPC_USER} ]; then
-    SOFTWARE_PREFIX="${SOFTWARE_PREFIX}${ROAMES_HPC_USER}"
+if [ ! -z ${ROAMES_HPC_USER} ]; then
+	echo "ROAMES_HPC_USER exists - ${ROAMES_HPC_USER}"
+    SOFTWARE_PREFIX="${SOFTWARE_PREFIX}/${ROAMES_HPC_USER}"
 fi
+
+echo "${ROAMES_HPC_USER}"
 echo "Deploying to ${SOFTWARE_PREFIX}"
 
 # 
@@ -40,12 +41,12 @@ for js_file in `ls dist.*.min.js`; do
     aws s3api get-object-acl --bucket roames-software --key ${SOFTWARE_PREFIX}/${js_file}
 
     # look for source maps - no version, just the dist.min.js.map as this is what is referenced in the source map link in bundle
-    mapfile="${js_file}.map"
-    echo "Looking for map file ${mapfile}"
+    #mapfile="dist.min.js.map"
+    echo "Looking for map file"
     if [ -f "dist.min.js.map" ]; then
         echo "The file exists"
         echo "copy to s3"
-        aws s3 cp ${mapfile} s3://roames-software/${SOFTWARE_PREFIX}/dist.min.js.map
+        aws s3 cp dist.min.js.map s3://roames-software/${SOFTWARE_PREFIX}/dist.min.js.map
 
         echo "public read ACL on object"
 	    aws s3api put-object-acl --bucket roames-software --key ${SOFTWARE_PREFIX}/dist.min.js.map --acl public-read
@@ -53,4 +54,4 @@ for js_file in `ls dist.*.min.js`; do
 
 done
 
-echo "Definitely done"
+echo "All done, Definitely"
